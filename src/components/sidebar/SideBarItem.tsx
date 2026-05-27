dadb2ceef22c48dc5fe01dfe8ae61667d86ce0f9
PrimalCat-Real <romanenko.denis2003@gmail.com>
2026-05-27 15:24:40 +0200

    try 2 fix navigation

--- src/components/sidebar/SideBarItem.tsx
+++ src/components/sidebar/SideBarItem.tsx
@@ -1,37 +1,38 @@
 'use client'
-import React, { useEffect, useState } from 'react'
-import Link from 'next/link'
-import type { SideItem } from './SideBar'
+import { usePathname, useRouter } from 'next/navigation'
 import { Button } from '@/modules/shadcn/components/ui/button'
 import { cn } from '@/modules/shadcn/lib/utils'
-import { usePathname } from 'next/navigation'
-
+import type { SideItem } from './SideBar'
 
 interface SideBarItemProps extends SideItem {
-    className?: string,
-    isActive?: boolean,
-    iconSize?: number,
-    onClick?: () => void,
+    className?: string
+    iconSize?: number
+    onClick?: () => void
 }
 
 const SideBarItem = ({ icon: Icon, label, link, className, iconSize, onClick }: SideBarItemProps) => {
+    const pathname = usePathname()
+    const router = useRouter()
+    const isActive = pathname === link || pathname === `${link}/`
 
-    const pathname = usePathname();
-    const [isActive, setActive] = useState<boolean>(false)
-
-    useEffect(() => {
-        setActive(pathname === link)
-    }, [pathname, link])
     return (
-        <Link href={link} onClick={onClick} title={label}>
-            <Button
-                size='icon-none'
-                variant="ghost"
-                className={cn("relative z-10 flex items-center justify-center p-0 transition-all duration-300 rounded-lg", !isActive && "text-primary", isActive && "bg-dim", className)}
-            >
-                <Icon style={{ width: iconSize, height: iconSize }} />
-            </Button>
-        </Link>
+        <Button
+            size='icon-none'
+            variant="ghost"
+            title={label}
+            className={cn(
+                "relative z-10 flex items-center justify-center p-0 transition-all duration-300 rounded-lg",
+                !isActive && "text-primary",
+                isActive && "bg-dim",
+                className,
+            )}
+            onClick={() => {
+                router.push(link)
+                onClick?.()
+            }}
+        >
+            <Icon style={{ width: iconSize, height: iconSize }} />
+        </Button>
     )
 }
 
